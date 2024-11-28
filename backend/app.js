32a46ab5aa0c05bf0804bc41954982b3338d36cd
axteure <clement.hanquiez@gmail.com>
2024-11-28 08:45:45 +0100

    tache cron exemple

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+var cron = require('node-cron');
+
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,6 +10,19 @@ app.use(cors());
 
 // Exemple de route
 app.get('/', (req, res) => res.send('Bienvenue au Hackathon'));
+
+app.get('/cron', (req, res) => {
+
+    cron.schedule('*/2 * * * * *', () => {
+        console.log('running a task every 2 seconds');
+    });
+
+    res.send('Cron déclenché.')
+
+});
+
+
+
 
 // Lancement du serveur
 app.listen(5000, () => console.log('Serveur backend démarré sur le port 5000'));
